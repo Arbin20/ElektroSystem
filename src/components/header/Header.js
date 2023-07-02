8794a273ee6d740346aafbebae8b63405bec640b
Arbin Qazimi <56562999+Arbin20@users.noreply.github.com>
2023-07-02 16:35:24 +0200

    Update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,27 +5,27 @@ import { Link } from 'react-router-dom';
 
 function Header() {
 
-  const scrollToTop = () => {
-    window.scrollTo(0, 0)
-  }
+    const scrollToTop = () => {
+        window.scrollTo(0, 0)
+    }
 
-  return (
-    <header className="header">
-      <div className="header-container">
-        <Link to='/'>
-          <img className="main-logo" src="/images/logo.png" alt="logo" onClick={scrollToTop} /> 
-        </Link>
-        <div className="info-container">
-          <div className="phone-container">
-            <i className="header-icon fas fa-phone-alt"></i>
-            <h4>Telefoni</h4>
-            <a className="mob" href="tel:071-722-453">071-722-453</a>
-          </div>
-        </div>
-      </div>
-      <Navbar scrollToTop={scrollToTop} />
-    </header>
-  );
+    return (
+        <header className="header">
+            <div className="header-container">
+                <Link to='/'>
+                    <img className="main-logo" src="/images/logo.png" alt="logo" onClick={scrollToTop} />
+                </Link>
+                <div className="info-container">
+                    <div className="phone-container">
+                        <i className="header-icon fas fa-phone-alt"></i>
+                        <h4>Telefoni</h4>
+                        <a className="mob" href="tel:070-356-113">+389 70 356 113</a>
+                    </div>
+                </div>
+            </div>
+            <Navbar scrollToTop={scrollToTop} />
+        </header>
+    );
 }
 
 export default Header;
