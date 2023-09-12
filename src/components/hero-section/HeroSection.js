d4e1c26887043b2b34431e2ce17f8547b280daf1
Arbin Qazimi <56562999+Arbin20@users.noreply.github.com>
2023-09-12 20:36:11 +0200

    update

--- src/components/hero-section/HeroSection.js
+++ src/components/hero-section/HeroSection.js
@@ -9,13 +9,13 @@ function HeroSection() {
         <div className="image-container">
           <img className="cta-image" src="/images/cta-image.jpg" alt="" />
           <div className="main-text-cont">
-            <h2 className="main-title">A-F Elektro</h2>
+            <h2 className="main-title">Elektro-Force Nerasht</h2>
             <h3 className="strapline">Nerasht</h3>
           </div>
         </div>
         <div className="areas-list-container">
           <div className="intro">
-            <h2 className="black-heading">A-F Elektro</h2>
+            <h2 className="black-heading">Elektro-Force Nerasht</h2>
             <p className="areas-text">Për të gjitha nevojat tuaja elektrike, ne përpiqemi të ofrojmë një shërbim miqësor dhe profesional, duke u krenuar me cilësinë e punës sonë. Mos ngurroni të kontaktoni në lidhje me çdo shqetësim që mund të keni në lidhje me energjinë elektrike. Do të donim të dëgjonim nga ju!</p>
           </div>
           <div className="areas">
--- src/components/modal-gallery/ModalGallery.js
+++ src/components/modal-gallery/ModalGallery.js
@@ -3,27 +3,34 @@ import './ModalGallery.css';
 
 function ModalGallery({ closeModal, projectObject: { largeimg, portrait } }) {
 
-  useEffect(() => {
-    console.log("first render");
-    // give it styles to position the site correctly. use old fashioned js here
-    const body = document.getElementsByTagName("body")[0];
-    body.classList.add('modal-open');
+    useEffect(() => {
+        console.log("first render");
+        // give it styles to position the site correctly. use old fashioned js here
+        const body = document.getElementsByTagName("body")[0];
+        body.classList.add('modal-open');
 
-    return function cleanup() {
-      console.log("unmount");
-      // remove those same styles
-      body.classList.remove('modal-open');
-    }
-  }, [])
+        return function cleanup() {
+            console.log("unmount");
+            // remove those same styles
+            body.classList.remove('modal-open');
+        }
+    }, [])
 
-  return (
-    <div className="modal-bg" onClick={closeModal}>
-      <div className={portrait ? "modal-img-cont-portrait" : "modal-img-cont"}>
-        <img className="modal-img" src={largeimg} alt="" />
-        <i className="fas fa-times close-btn" onClick={closeModal}></i>
-      </div>
-    </div>
-  );
+    return ( <
+        div className = "modal-bg"
+        onClick = { closeModal } >
+        <
+        div className = { portrait ? "modal-img-cont-portrait" : "modal-img-cont" } >
+        <
+        img className = "modal-img"
+        src = { largeimg }
+        alt = "" / >
+        <
+        i className = "fas fa-times close-btn"
+        onClick = { closeModal } > < /i> <
+        /div> <
+        /div>
+    );
 }
 
 export default ModalGallery;
